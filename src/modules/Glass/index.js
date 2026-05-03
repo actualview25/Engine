a6ee44c500f048f3ae9af65d3e61e0acca9d3045
actualview25 <actual.view25@gmail.com>
2026-05-03 13:21:38 -0700

    Create index.js

---
+++ src/modules/Glass/index.js
@@ -0,0 +1,21 @@
+// ============================================
+// GLASS MODULE - INDEX
+// ============================================
+
+export { Glass } from './Glass.js';
+export { WindowGlass } from './WindowGlass.js';
+export { CurtainWall } from './CurtainWall.js';
+export { GlassPartition } from './GlassPartition.js';
+export { Skylight } from './Skylight.js';
+export { GlassFloor } from './GlassFloor.js';
+export { GlassRailing } from './GlassRailing.js';
+export { StainedGlass } from './StainedGlass.js';
+export { SmartGlass } from './SmartGlass.js';
+export { GlassBlock } from './GlassBlock.js';
+export { GlassMaterial } from './Material.js';
+
+// Global exports
+export { GlobalGlass } from './global/GlobalGlass.js';
+export { GlobalSkylight } from './global/GlobalSkylight.js';
+export { GlobalCurtainWall } from './global/GlobalCurtainWall.js';
+export { GlobalGlassPartition } from './global/GlobalGlassPartition.js';
