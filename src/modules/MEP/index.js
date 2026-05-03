7a56d5db5854fecae6bfde57ea45fd51d6b30a87
actualview25 <actual.view25@gmail.com>
2026-05-03 12:16:44 -0700

    Create index.js

---
+++ src/modules/MEP/index.js
@@ -0,0 +1,17 @@
+// ============================================
+// MEP MODULE - INDEX
+// ============================================
+
+export { ElectricalCircuit } from './Electrical.js';
+export { PlumbingSystem } from './Plumbing.js';
+export { HVACSystem } from './HVAC.js';
+export { Cable } from './Cable.js';
+export { Pipe } from './Pipe.js';
+export { Fixture } from './Fixture.js';
+export { Drainage } from './Drainage.js';
+export { MEPMaterial } from './Material.js';
+
+// Global exports
+export { GlobalElectrical } from './global/GlobalElectrical.js';
+export { GlobalHVAC } from './global/GlobalHVAC.js';
+export { GlobalPlumbing } from './global/GlobalPlumbing.js';
