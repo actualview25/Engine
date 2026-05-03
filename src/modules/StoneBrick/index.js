8ecf7bc41b504e19c51334b068b5d09712affb59
actualview25 <actual.view25@gmail.com>
2026-05-03 13:23:47 -0700

    Create StoneBrick module index file

---
+++ src/modules/StoneBrick/index.js
@@ -0,0 +1,19 @@
+// ============================================
+// STONE & BRICK MODULE - INDEX
+// ============================================
+
+export { Stone } from './Stone.js';
+export { Brick } from './Brick.js';
+export { Marble } from './Marble.js';
+export { Granite } from './Granite.js';
+export { Tile } from './Tile.js';
+export { Cladding } from './Cladding.js';
+export { Pavement } from './Pavement.js';
+export { StoneBrickMaterial } from './Material.js';
+
+// Global exports
+export { GlobalStone } from './global/GlobalStone.js';
+export { GlobalBrick } from './global/GlobalBrick.js';
+export { GlobalMarble } from './global/GlobalMarble.js';
+export { GlobalCladding } from './global/GlobalCladding.js';
+export { GlobalPavement } from './global/GlobalPavement.js';
