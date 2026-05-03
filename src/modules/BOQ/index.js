0d2c9ddf0de92d6553b03511a845717deb1f5384
actualview25 <actual.view25@gmail.com>
2026-05-03 13:24:34 -0700

    Create index.js

---
+++ src/modules/BOQ/index.js
@@ -0,0 +1,14 @@
+// ============================================
+// BOQ MODULE - INDEX
+// ============================================
+
+export { BOQCalculator } from './Calculator.js';
+export { BOQExporter } from './Exporter.js';
+export { BOQReporter } from './Reporter.js';
+export { BOQCategories } from '../boq/BOQCategories.js';
+export { BOQManager } from '../boq/BOQManager.js';
+
+// Global exports (من مجلد global)
+export { GlobalBOQCalculator } from './global/GlobalBOQCalculator.js';
+export { GlobalReporter } from './global/GlobalReporter.js';
+export { GlobalEarthworksBOQ } from './global/GlobalEarthworksBOQ.js';
