d856c6cfdeae6f6faaa5dd53449f593cb59b632e
actualview25 <actual.view25@gmail.com>
2026-05-03 13:25:36 -0700

    Create index.js

---
+++ src/modules/Earthworks/index.js
@@ -0,0 +1,12 @@
+// ============================================
+// EARTHWORKS MODULE - INDEX
+// ============================================
+
+export { Excavation } from './Excavation.js';
+export { Compaction } from './Compaction.js';
+export { Layer } from './Layer.js';
+export { SoilMaterial } from './SoilMaterial.js';
+
+// Global exports
+export { GlobalExcavation } from './global/GlobalExcavation.js';
+export { GlobalCompaction } from './global/GlobalCompaction.js';
