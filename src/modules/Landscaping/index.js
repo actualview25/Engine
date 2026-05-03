84449683efab0e77c63616d5e6b6a81059f9be4e
actualview25 <actual.view25@gmail.com>
2026-05-03 13:22:38 -0700

    Create Landscaping module index file with exports

---
+++ src/modules/Landscaping/index.js
@@ -0,0 +1,18 @@
+// ============================================
+// LANDSCAPING MODULE - INDEX
+// ============================================
+
+export { Plant } from './Plant.js';
+export { Tree } from './Tree.js';
+export { Grass } from './Grass.js';
+export { Palm } from './Palm.js';
+export { Fountain } from './Fountain.js';
+export { GardenLight } from './GardenLight.js';
+export { GardenPath } from './GardenPath.js';
+export { LandscapingMaterial } from './Material.js';
+
+// Global exports
+export { GlobalFountain } from './global/GlobalFountain.js';
+export { GlobalGardenPath } from './global/GlobalGardenPath.js';
+export { GlobalPlant } from './global/GlobalPlant.js';
+export { GlobalTree } from './global/GlobalTree.js';
